2791631c53c596d74c5344edf9942e0bc004ff27
Disha Malhotra <143073189+technoenthus@users.noreply.github.com>
2025-03-22 10:56:04 +0530

    Time: 13 ms (41.61%) | Memory: 45 MB (84.76%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -48,14 +48,14 @@ class Solution {
     }
 
     void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX == rootY) return;
+        int rootX=find(x);
+        int rootY=find(y);
+        if (rootX==rootY) return;
 
-        if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else if (rank[rootX] > rank[rootY]) {
-            parent[rootY] = rootX;
+        if (rank[rootX]<rank[rootY]) {
+            parent[rootX]=rootY;
+        } else if (rank[rootX]>rank[rootY]) {
+            parent[rootY]=rootX;
         } else {
             parent[rootY] = rootX;
             rank[rootX]++;
