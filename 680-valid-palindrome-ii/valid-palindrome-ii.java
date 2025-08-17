7d21afc4189bb03d9641880e772d6a9c4c8dc903
Disha Malhotra <143073189+technoenthus@users.noreply.github.com>
2025-08-17 18:51:20 +0530

    Time: 4 ms (99.58%) | Memory: 45.5 MB (41.04%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+
+            if (s.charAt(left) != s.charAt(right)){
+                return isPal(s, left + 1, right) || isPal(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+        
+    }
+
+    public boolean isPal(String s, int left, int right){
+        while(left < right){
+
+            if (s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
